7df37daa8d5f3205785c32a0b9aef38218ae5b19
hostolaza25 <hostolaza25@izarraitz.eus>
2026-05-04 11:46:52 +0200

    While begizta
    kontagailua++ gehitu da begizta amaigabea ekiditeko.

--- Kontrol Puntua/AkatsakKP.cs
+++ Kontrol Puntua/AkatsakKP.cs
@@ -22,10 +22,11 @@ Console.WriteLine($"2. Batez bestekoa: {batazBestekoa}");
 // 2. AKATSA
 int kontagailua = 0;
 
-while (kontagailua < 5)
-{
-    Console.WriteLine($"4. Kontagailua: {kontagailua}");
-}
+            while (kontagailua < 5)
+            {
+                Console.WriteLine($"2. Kontagailua: {kontagailua}");
+                kontagailua++; 
+            }
 
 
 // 3. AKATSA
